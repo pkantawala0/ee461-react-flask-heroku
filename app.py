adf967c62bdf238bf89fd530497512494a6f8842
Purva Kantawala <p.kantawala0@utexas.edu>
2022-10-30 11:25:52 -0500

    More updates

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask, send_from_directory, render_template, request
 
-app = Flask(__name__, static_url_path='', static_folder='ui/build/') 
-#, template_folder = 'ui/templates/
+app = Flask(__name__, static_url_path='', static_folder='ui/build/', template_folder = 'ui/templates/')
 
 @app.route('/')
 def index():
@@ -14,36 +13,36 @@ def params():
     projectID = request.args.get('projectID')
     qty = request.args.get('qty')
 
-# @app.route('/<projectID>/<qty>')
-# def checkIn_hardware(qty):
-#     #data = {'projectID': projectID, 'qty': qty}
-#     projectID = request.args.get('projectID')
-#     qty = request.args.get('qty')
-#     return render_template('settings.html', qty=qty)
+@app.route('/<projectID>/<qty>')
+def checkIn_hardware(qty):
+    #data = {'projectID': projectID, 'qty': qty}
+    projectID = request.args.get('projectID')
+    qty = request.args.get('qty')
+    return render_template('settings.html', qty=qty)
 
-# @app.route('/<projectID>/<qty>', methods=["POST", "GET"])
-# def checkOut_hardware(projectID, qty):
-#     projectID = request.args.get('projectID')
-#     qty = request.args.get('qty')
-#     return projectID, qty
+@app.route('/<projectID>/<qty>', methods=["POST", "GET"])
+def checkOut_hardware(projectID, qty):
+    projectID = request.args.get('projectID')
+    qty = request.args.get('qty')
+    return projectID, qty
 
-# @app.route('/<projectID>/<qty>', methods=["POST", "GET"])
-# def joinProject(projectID):
-#     projectID = request.args.get('projectID')
-#     qty = request.args.get('qty')
-#     return projectID
+@app.route('/<projectID>/<qty>', methods=["POST", "GET"])
+def joinProject(projectID):
+    projectID = request.args.get('projectID')
+    qty = request.args.get('qty')
+    return projectID
 
-# @app.route('/<projectID>/<qty>', methods=["POST", "GET"])
-# def leaveProject(projectID):
-#     projectID = request.args.get('projectID')
-#     qty = request.args.get('qty')
-#     return projectID
+@app.route('/<projectID>/<qty>', methods=["POST", "GET"])
+def leaveProject(projectID):
+    projectID = request.args.get('projectID')
+    qty = request.args.get('qty')
+    return projectID
 
-# @app.errorhandler(404)
-# def not_found(e):
-#         projectID = request.args.get('projectID')
-#     qty = request.args.get('qty')
-#     return app.send_static_file('index.html')
+@app.errorhandler(404)
+def not_found(e):
+    projectID = request.args.get('projectID')
+    qty = request.args.get('qty')
+    return app.send_static_file('index.html')
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
--- ui/src/Project.js
+++ ui/src/Project.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import {useState} from 'react';
+import Switch from '@mui/material/Button';
 // import qty from 'ui\templates\settings.html';
 // import projectID from 'ui\templates\settings.html';
 
@@ -21,26 +22,42 @@ class Project extends React.Component {
       {
         projectNum: props.num,
         HWSet1: props.hw1,
-        HWSet2: props.hw2
+        HWSet2: props.hw2,
+        // label: "Join"
       };
 
     //   this.updateState = this.updateState.bind(this)
 
-      this.handleChange = this.handleChange.bind(this);
       this.chkInHW1 = this.chkInHW1.bind(this);
       this.chkInHW2 = this.chkInHW2.bind(this);
+      this.enableEdit = this.enableEdit.bind(this);
 
       this.chkOutHW1 = this.chkOutHW1.bind(this);
       this.chkOutHW2 = this.chkOutHW2.bind(this);
     //   this.handleSubmit = this.handleSubmit.bind(this);
     }
     
+    enableEdit()
+    {   
+        let temp = "NA"
+        if(prevState => "Join")
+        {
+            this.setState({disabled: !this.state.disabled,
+                label: "Leave"});
+        }
+        else
+        {
+            this.setState({disabled: !this.state.disabled,
+                label: "Join"});
+
+        }
+    }  
 
     chkInHW1()
     {   
-         this.setState(prevState => {
+        this.setState(prevState => {
             return {HWSet1: (parseInt(prevState.HWSet1) + 1) + ""}
-         });
+        });
     }
 
     chkInHW2()
@@ -62,10 +79,6 @@ class Project extends React.Component {
         this.setState(prevState => {
             return {HWSet2: (parseInt(prevState.HWSet2) - 1) + ""}
          });
-    }
-
-    handleChange(event) {
-      this.setState({value: event.target.value});
     }
 
     render() {
@@ -141,7 +154,10 @@ class Project extends React.Component {
            </div>
            <div class = "Join">
             <div class = "Buttons">
-              <Button variant="text" onClick={() => { alert("Joined "); }} >
+              <Button variant="text" label = {this.state.label} onClick={() => {
+                alert("Joined " + this.state.projectNum);
+                {this.enableEdit}
+                }} >
                 Join
               </Button> 
             </div>   
@@ -151,6 +167,7 @@ class Project extends React.Component {
       )
     }
   }
+
 
 // ReactDOM.render(
 //   <App />,
